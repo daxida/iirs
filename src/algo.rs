cd5885e9726a3a10e586183b49e26a400429b4ed
daxida <galmaw@hotmail.com>
2024-03-01 10:34:09 +0100

    comprehension changes to algo.rs

--- src/algo.rs
+++ src/algo.rs
@@ -3,13 +3,6 @@ use crate::{
     rmq::{Rmq, Sparse},
 };
 
-// Calculates the Longest Common Prefix array of a text and stores value in given variable LCP
-//
-// INPUT:
-// - Text
-// - Text length
-// - Suffix Array
-// - Longest Common Prefix data structure (empty)
 pub fn lcp_array(s: &[u8], s_n: usize, sa: &[i64], inv_sa: &[usize]) -> Vec<usize> {
     let mut lcp: Vec<usize> = vec![0; s_n];
     let mut j: usize;
@@ -30,25 +23,20 @@ pub fn lcp_array(s: &[u8], s_n: usize, sa: &[i64], inv_sa: &[usize]) -> Vec<usiz
     lcp
 }
 
-// Calculates a list of Longest Common Extensions, corresponding to 0, 1, 2, etc. allowed mismatches, up to maximum number of allowed mismatches
+// Calculates a list of Longest Common Extensions, corresponding to 0, 1, 2, etc. allowed mismatches, 
+// up to maximum number of allowed mismatches.
 //
 // EXTRA INFO:
 // - Only considers "real" mismatches (degenerate string mismatching according to IUPAC character matrix)
 // - Takes into account the matching possibility of non A, C, G, T/U characters
-// - Longest Common Extension calculated from positions i and j (order of i, j input does not matter)
-// - Only starts counting number of allowed mismatches that occur after the given initial gap, however earlier mismatches are still stored
-// - Should only be used after MatchMatrix has been instantiated with necessary data
+// - Longest Common Extension calculated from positions i and j
+// - Only starts counting number of allowed mismatches that occur after the given initial gap, 
+//   however earlier mismatches are still storeds
+// 
+// - Kangaroo algorithm. A simple explanation can be found here: https://www.youtube.com/watch?v=Njv_q9RA-hs
+// - For the BANANA case, the given (i, j) will be:
+//     (1, 13), (1, 12), (2, 12), (2, 11), (3, 11) ... (6, 8)
 //
-// INPUT:
-// - Text
-// - Indexes i and j
-// - Text length
-// - Inverse Suffix Array
-// - Longest Common Prefix Array (LCP)
-// - Data structure (filled) with preprocessed values to perform Range Minimum Queries (Type 1: 'A', Type 2: 'rmq')
-// - Maximum number of allowed mismatches
-// - Initial gap
-// - Data structure to store resulting mismatch locations
 #[allow(clippy::too_many_arguments)]
 fn real_lce_mismatches(
     s: &[u8],
@@ -61,11 +49,6 @@ fn real_lce_mismatches(
     initial_gap: i32,
     matrix: &MatchMatrix,
 ) -> Vec<i32> {
-    // Kangaroo algorithm. A simple explanation can be found here:
-    // https://www.youtube.com/watch?v=Njv_q9RA-hs
-    //
-    // For the BANANA case, the given (i, j) will be:
-    // (1, 13), (1, 12), (2, 12), (2, 11), (3, 11) ... (6, 8)
     let mut mismatch_locs = vec![-1]; // Originally LinkedList<i32>
     let mut real_lce = 0;
 
@@ -101,19 +84,9 @@ fn real_lce_mismatches(
 
 // Finds all inverted repeats (palindromes) with given parameters and adds them to an output set
 //
-// INPUT:
-// - Data structure (set of integer 3-tuples) to store palindromes in form (left_index, right_index, gap)
-// - S = text + '$' + complement(reverse(text) + '#'
-// - Length of S
-// - Text length
-// - Inverse Suffix Array
-// - Longest Common Prefix Array (LCP)
-// - Data structure (filled) with preprocessed values to perform Range Minimum Queries (Type 1: 'A', Type 2: 'rmq')
-// - Tuple of parameters for palindromes to be found (minimum_length, maximum_length, maximum_allowed_number_of_mismatches, maximum_gap)
-//
 // NOTES:
-// - The original algorithm returned a set of tuples. The use of the corresponding BTreeSet<(i32, i32, i32)
-//   here is marginally slower (compared to Vec<(i32, i32, i32)>, while making the code less clear.
+// - The original algorithm returned a set of tuples: BTreeSet<(i32, i32, i32)> but did no sorting.
+//   It was marginally slower (compared to Vec<(i32, i32, i32)>, while making the code less clear.
 //   >> AT NO POINT IS A DUPLICATE pushed into "palindromes".
 // - If we use instead a Vec<(i32, i32, 32)> the collection needs to be returned sorted if the data will be printed sorted
 //   afterwards in "format". The palindromes found are the same without sorting, they are just not returned in the expected order.
@@ -135,12 +108,16 @@ pub fn add_palindromes(
     let is_max_gap_odd = max_gap % 2 == 1;
 
     for c in (0..=2 * (n - 1)).map(|c| (c as f64) / 2.0) {
+        // Determine if value of centre corresponds to an odd or even palindrome
         let is_odd = c.fract() == 0.0;
 
+        // The following part is equivalent to:
+        // let margin = c.fract();
+        // let (i, j) = (1.0 + c - margin, behind - c - margin);
         let (i, j) = if is_odd {
-            (c + 1.0, behind - c)
+            ((c + 1.0) as usize, (behind - c) as usize)
         } else {
-            (c + 0.5, behind - (c + 0.5))
+            ((c + 0.5) as usize, (behind - (c + 0.5)) as usize)
         };
 
         let initial_gap = if is_max_gap_odd {
@@ -153,8 +130,8 @@ pub fn add_palindromes(
 
         let mismatch_locs = real_lce_mismatches(
             s,
-            i as usize,
-            j as usize,
+            i,
+            j,
             s_n,
             inv_sa,
             rmq,
@@ -208,6 +185,12 @@ pub fn add_palindromes(
                 start = valid_start_locs[start_it_ptr];
                 mismatch_diff = end.1 - start.1 - 1;
             }
+            
+            let start_mismatch = (start.0 + 1) as usize;
+            // Skip this iteration if the start mismatch chosen is such that the gap is not within the acceptable bound
+            if start_mismatch as i32 > initial_gap {
+                break;
+            }
 
             // While mismatch difference is within acceptable bound,
             // move end location to the right until mismatch difference becomes unacceptable
@@ -220,18 +203,22 @@ pub fn add_palindromes(
                 mismatch_diff = end.1 - start.1 - 1;
             }
 
-            let start_mismatch = (start.0 + 1) as usize;
-            // // Skip this iteration if the start mismatch chosen is such that the gap is not within the acceptable bound
-            if start_mismatch as i32 > initial_gap {
-                break;
-            }
             let end_mismatch = valid_end_locs[end_it_ptr - 1].0;
+            
+            let palindrome_length = end_mismatch as usize - start_mismatch;
+            if palindrome_length < min_len {
+                start_it_ptr += 1;
+                continue;
+            }
 
             // The following part is equivalent to:
             // let margin = c.fract();
             // let left  = (c + margin) as usize - end_mismatch;
             // let right = (c - margin) as usize + end_mismatch;
             // let gap   = 2 * start_mismatch + 1 - (margin * 2.0) as usize;
+            //
+            // NOTE: we find again that:
+            // let palindrome_length = (right - left + 1 - gap) / 2;
             let (left, right, gap): (usize, usize, usize);
 
             if is_odd {
@@ -244,39 +231,28 @@ pub fn add_palindromes(
                 gap = 2 * start_mismatch;
             }
 
-            // println!("(left, gap, right) = {} {} {}", left, right, gap);
+            // Check that potential palindrome is not too long
+            let palindrome = if palindrome_length <= max_len {
+                // Palindrome is not too long, so add to output
+                (left, right, gap)
+            } else {
+                // Palindrome is too long, so attempt truncation
+                let prev_end_mismatch_ptr = (end_it_ptr as i32 - 2).max(0) as usize;
+                let prev_end_mismatch = valid_end_locs[prev_end_mismatch_ptr].0;
+                let mismatch_gap = end_mismatch - prev_end_mismatch - 1;
+                let overshoot = palindrome_length - max_len;
 
-            // Check that potential palindrome is not too short
-            let palindrome_length = (right - left + 1 - gap) / 2;
-            // (c + end) - (c - end) + 1 - (2 * start + 2 + 1) / 2
-            // (2 * end + 1 - 2 * start - 3) / 2
-            // end - start - 1
-            // assert_eq!(palindrome_length, end_mismatch - start_mismatch - 1);
-
-            if palindrome_length >= min_len {
-                // Check that potential palindrome is not too long
-                let palindrome = if palindrome_length <= max_len {
-                    // Palindrome is not too long, so add to output
-                    (left, right, gap)
+                // Check if truncation results in the potential palindrome ending in a mismatch
+                if overshoot != mismatch_gap as usize {
+                    // Potential palindrome does not end in a mismatch, so add to output
+                    (left + overshoot, right - overshoot, gap)
                 } else {
-                    // Palindrome is too long, so attempt truncation
-                    let prev_end_mismatch_ptr = (end_it_ptr as i32 - 2).max(0) as usize;
-                    let prev_end_mismatch = valid_end_locs[prev_end_mismatch_ptr].0;
-                    let mismatch_gap = end_mismatch - prev_end_mismatch - 1;
-                    let overshoot = palindrome_length - max_len;
-
-                    // Check if truncation results in the potential palindrome ending in a mismatch
-                    if overshoot != mismatch_gap as usize {
-                        // Potential palindrome does not end in a mismatch, so add to output
-                        (left + overshoot, right - overshoot, gap)
-                    } else {
-                        // Potential palindrome does end in a mismatch, so truncate an additional 1
-                        // character either side then add to output
-                        (left + overshoot + 1, right - overshoot - 1, gap)
-                    }
-                };
-                palindromes.push(palindrome);
-            }
+                    // Potential palindrome does end in a mismatch, so truncate an additional 1
+                    // character either side then add to output
+                    (left + overshoot + 1, right - overshoot - 1, gap)
+                }
+            };
+            palindromes.push(palindrome);
 
             start_it_ptr += 1;
         }
